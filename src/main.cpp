9aa7f844ae414da567e25cd9081858c7e7f1dc8e
Dominik Salvet <dominik.salvet@gmail.com>
2022-04-20 11:36:05 +0200

    add hello world

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// author: Dominik Salvet
+// login: xsalve03
+// date: 2022-04-20
+// filename: main.cpp
+// summary: Simple Huffman codec with multiple options.
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, world!\n";
+}
